d8dfde77a46ba70e586a6ee96f42fe5c4c148386
stxRand <marcin.kowiel@stxnext.pl>
2013-06-20 11:32:20 +0200

    fill setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "PythonPath",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Marcin Kowiel",
+    author_email = "marcin.kowiel@stxnext.pl",
+    description = "Python Path Project",
+)
  
