2a2a86fe37c6919edbdc4d2be727958dd46d061b
stxRand <marcin.kowiel@stxnext.pl>
2013-07-01 15:28:23 +0200

    reorganizing setup

--- pythonpath/settings.py
+++ pythonpath/settings.py
@@ -1,13 +1,11 @@
 
 
-NOKAUT_KEY = ''
-
 """number that indicates how many days we would like
 to get queries and statisics of Search entities in the database
 """
 DATABASE_EXPIRE_NUMBER_OF_DAYS = 3
 
 try:
-    from settings_local import *
+    from settings_local import NOKAUT_KEY
 except ImportError:
     pass
